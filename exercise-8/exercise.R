1f54e8b47c14b64688333f4e9cb253a9a878e2f7
KATIE BRELAND <KATIEBRE@UW.EDU>
2017-04-19 20:00:07 -0700

    completed participation #8 which had us use dplyr to work with a data set dealing with Pulitzer prizes

--- exercise-8/exercise.R
+++ exercise-8/exercise.R
@@ -1,36 +1,47 @@
 # Exercise 8: Pulitzer Prizes
 
 # Read in the data
-pulitzer <- read.csv("data/pulitzer-circulation-data.csv", stringsAsFactors = FALSE)
+pulitzer <- read.csv("~/desktop/info/m11-dplyr/m11-dplyr/exercise-8/data/pulitzer-circulation-data.csv", stringsAsFactors = FALSE)
 
 # Install and load the needed libraries
 # Be sure to comment out the install.packages function so it won't install it every time it runs
 # Remeber you only need to install a package once
-#install.packages(dplyr)
+install.packages('dplyr')
 library(dplyr)
 
 # View in the data set. Start to understand what the data columns contains
 # Be sure to comment out the function so it won't view everytime you run the code.
-
+View(pulitzer)
 
 # Use 'colnames' to print out the names of the columns
-
+print(colnames(pulitzer))
 
 # Use 'str' to print what types of values are contained in each column
 # Did any value type surprise you? Why do you think they are that type?
-
+print(str(pulitzer))
+#I was surprised that some of the words are stored as characters instead of strings. I think they are characters because characters are easier to sort through as opposed to entire strings.
 
 # Add a column in a dataframe called 'Pulitzer.Prize.Change` that contains the diffrence in changes
 # in Pulitzer Prize Winners from 2004 to 2013 and Pultizer Prize Winners from 1990 to 2003.
-
+Pulitzer.Prize.Change <- mutate(pulitzer, diff.2004.2014.to.1990.2003 = Pulitzer.Prize.Winners.and.Finalists..2004.2014 -Pulitzer.Prize.Winners.and.Finalists..1990.2003)
 
 # What publication gained the most pulitzer prizes from 2004-2014?
 # Be sure to use the pipe operator! 
-
+most.2014 <- filter(pulitzer, max(Pulitzer.Prize.Winners.and.Finalists..2004.2014) == 
+                      Pulitzer.Prize.Winners.and.Finalists..2004.2014) %>% 
+  select(Newspaper)
 
 # Which publication with at least 5 Pulitzers won from 2004-2014 had the biggest decrease(negative) in Daily circulation numbers? 
 # This publication should have Pulitzer prizes won a minimum of 5 Pulitzers, as well as the biggest decrease in circulation
-
+most.award.winning.decrease <- filter(pulitzer, Pulitzer.Prize.Winners.and.Finalists..2004.2014 >= 5) %>% 
+  filter(min(Change.in.Daily.Circulation..2004.2013 ) == Change.in.Daily.Circulation..2004.2013) %>% 
+  select(Newspaper)
 
 # Your turn! An important part about being a data scientist is asking questions. 
 # Create a question and use dplyr to figure out the answer.  
+#Question: Which newspaper won the least Pulitzers from 2004-2014?
+least.amount.of.wins<- filter(pulitzer, min(Pulitzer.Prize.Winners.and.Finalists..2004.2014) == 
+                                Pulitzer.Prize.Winners.and.Finalists..2004.2014) %>% 
+  select(Newspaper)
+
+
